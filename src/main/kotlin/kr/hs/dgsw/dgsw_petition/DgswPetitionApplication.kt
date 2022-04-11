de5158e22d7b76c4bf6f4bf34ac64be82aef68f8
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-04-11 15:42:15 +0900

    Project Settings

---
+++ src/main/kotlin/kr/hs/dgsw/dgsw_petition/DgswPetitionApplication.kt
@@ -0,0 +1,11 @@
+package kr.hs.dgsw.dgsw_petition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DgswPetitionApplication
+
+fun main(args: Array<String>) {
+	runApplication<DgswPetitionApplication>(*args)
+}
---
+++ src/test/kotlin/kr/hs/dgsw/dgsw_petition/DgswPetitionApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.dgsw_petition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DgswPetitionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
