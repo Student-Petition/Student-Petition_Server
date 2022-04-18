86a67381865dbd81b59e0f2e093fdcb75dd99185
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-04-18 14:53:46 +0900

    Update : Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,12 +25,17 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	compileOnly("org.projectlombok:lombok")
+
 	runtimeOnly("mysql:mysql-connector-java")
+
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
